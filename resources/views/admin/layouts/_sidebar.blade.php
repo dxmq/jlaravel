d05a98b9c692a3495507655181fe17d3a40ab534
h1ml <1697859639@qq.com>
2019-04-15 18:17:59 +0800

    add active to li

--- resources/views/admin/layouts/_sidebar.blade.php
+++ resources/views/admin/layouts/_sidebar.blade.php
@@ -20,9 +20,9 @@
                     <span class="pull-right-container"></span>
                 </a>
                 <ul class="treeview-menu">
-                    <li><a href="/admin/permissions"><i class="fa fa-circle-o"></i> 权限管理</a></li>
-                    <li><a href="/admin/users"><i class="fa fa-circle-o"></i> 用户管理</a></li>
-                    <li><a href="/admin/roles"><i class="fa fa-circle-o"></i> 角色管理</a></li>
+                    <li class="{{ isActiveURL('/admin/permissions') }}"><a href="/admin/permissions"><i class="fa fa-circle-o"></i> 权限管理</a></li>
+                    <li class="{{ isActiveURL('/admin/users') }}"><a href="/admin/users"><i class="fa fa-circle-o"></i> 用户管理</a></li>
+                    <li class="{{ isActiveURL('/admin/roles') }}"><a href="/admin/roles"><i class="fa fa-circle-o"></i> 角色管理</a></li>
                 </ul>
             </li>
             @endcan
